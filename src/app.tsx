5826fc3c934ddbc6a69dbecbaade0945a181a7f4
R-Sawai <sawai.reo@gmail.com>
2025-08-31 10:18:44 +0900

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import type { FC } from "react";
+
+export const App: FC = () => {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-50">
+      <div className="p-8 rounded-2xl shadow-md bg-white">
+        <h1 className="text-3xl font-bold">Hello, World! 🌍</h1>
+      </div>
+    </main>
+  );
+};
